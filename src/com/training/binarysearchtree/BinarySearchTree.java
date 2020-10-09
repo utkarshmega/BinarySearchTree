72b40860a67d8c087760c9a972d608e6138eaab7
utkarshmega <utkarsheduc@gmail.com>
2020-10-09 22:28:12 +0530

    Welcome to the binary search tree

---
+++ src/com/training/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,11 @@
+package com.training.binarysearchtree;
+
+public class BinarySearchTree{
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Binary Search Tree Implementation");
+		
+	}
+
+}
